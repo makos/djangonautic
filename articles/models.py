734d749a647c4188579e4f175437f7d0d8b452eb
Mateusz Makowski <matmakos@gmail.com>
2018-06-22 15:35:24 +0200

    initial commit

---
+++ articles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+	title = models.CharField(max_length = 100)
+	slug = models.SlugField()
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add = True)
+	# add in thumbnail
+	# add in author
+
+	def __str__(self):
+		return self.title
+
+	def snippet(self):
+		if len(self.body) < 50:
+			return self.body
+		else:
+			return self.body[:50] + "..."
